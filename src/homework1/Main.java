e7dba2bd40680c269b5dbd7458a5824e04474fb2
Egor <egor-zatonskii@yandex.ru>
2021-01-29 20:37:08 +0400

    404

--- src/homework1/Main.java
+++ src/homework1/Main.java
@@ -3,6 +3,36 @@ package homework1;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello, World!");
+
+        byte b1 = 10;
+        short s1 = 500;
+        int i1 = 9000;
+        long l1 = 88888888L;
+
+        float f1 = 120.0f;
+        double d1 = 15.4;
+
+        boolean bool = true;
+        char c1 = 'a';
+
+
+//        int a = 10;
+//        if (a > 0 && a % 5 == 0) {
+//            System.out.println("a больше 0");
+//        } else  {
+//            System.out.println("a не больше 0");
+//        }
+
+//        public static int float result = calculate {
+//            calculate a * (b + (c / d));
+//            int a = 10;
+//            int b = 5;
+//            int c = 12;
+//            int d = 6;
+//
+//            System.out.println(result);
+        System.out.println("Hello, World!");
+
     }
 }
